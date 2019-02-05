564f252081031db5bb6cb121fc93ce255e36758d
yajac <yajac.com@gmail.com>
2019-02-04 21:12:20 -0500

    Use values from body

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/yajac/coffee-maker-3000/slack"
+	"net/url"
 )
 
 // Handler is executed by AWS Lambda in the main function. Once the request
@@ -13,13 +13,22 @@ func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 
 	fmt.Printf("Request: %v\n", request)
 
-	iotResponse, err := slack.HandleIOTEvent()
+	fmt.Printf("Request Body: %v\n", request.Body)
 
-	fmt.Printf("IOTResponse: %v\n", iotResponse)
-
+	values, err := url.ParseQuery(request.Body)
 	if err != nil {
 		return events.APIGatewayProxyResponse{}, err
 	}
+
+	fmt.Printf("Values: %v\n", values)
+
+	command := values["command"]
+
+	fmt.Printf("Command: %v\n", command)
+
+	text := values["text"]
+
+	fmt.Printf("Text: %v\n", text)
 
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
